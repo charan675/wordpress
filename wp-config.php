65439296c87361db44c19f6e07fa0f69fd5d3490
charan675 <65114117+charan675@users.noreply.github.com>
2023-06-14 15:05:11 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'admin' );
 define( 'DB_PASSWORD', 'admin123' );
 
 /** Database hostname */
-define( 'DB_HOST', 'my-database.cwkkmddtlm1h.us-east-1.rds.amazonaws.com' );
+define( 'DB_HOST', 'database-1.cwkkmddtlm1h.us-east-1.rds.amazonaws.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
